7b1b88487b1c8a0b930776954ba4ed93a6adabe0
Josh Pollock <jpollock412@gmail.com>
2019-06-26 11:03:35 -0400

    forms page

---
+++ pages/forms.js
@@ -0,0 +1,11 @@
+import App from "../components/App";
+import Header from "../components/Header";
+
+export default () => (
+  <App>
+    <Header />
+    <article>
+      <h1>Forms</h1>
+    </article>
+  </App>
+);
