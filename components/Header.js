b10cb1e8f26cefe3bd0893c38722010c9f8ec45f
Josh Pollock <jpollock412@gmail.com>
2019-06-26 13:38:17 -0400

    get working with the older app compoennts

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,31 @@
-import Link from "next/link";
-import { withRouter } from "next/router";
+import Link from 'next/link';
+import { withRouter } from 'next/router';
 
 const Header = ({ router: { pathname } }) => (
-  <header>
-    <Link prefetch href="/">
-      <a className={pathname === "/" ? "is-active" : ""}>Home</a>
-    </Link>
-    <Link prefetch href="/forms">
-      <a className={pathname === "/forms" ? "is-active" : ""}>Forms</a>
-    </Link>
-    <style jsx>{`
-      header {
-        margin-bottom: 25px;
-      }
-      a {
-        font-size: 14px;
-        margin-right: 15px;
-        text-decoration: none;
-      }
-      .is-active {
-        text-decoration: underline;
-      }
-    `}</style>
-  </header>
+	<header>
+		<Link prefetch href="/">
+			<a className={pathname === '/' ? 'is-active' : ''}>Home</a>
+		</Link>
+		<Link prefetch href="/forms">
+			<a className={pathname === '/forms' ? 'is-active' : ''}>Forms</a>
+		</Link>
+		<a className={''} href="http://localhost:5101/graphql">
+			graphql
+		</a>
+		<style jsx>{`
+			header {
+				margin-bottom: 25px;
+			}
+			a {
+				font-size: 14px;
+				margin-right: 15px;
+				text-decoration: none;
+			}
+			.is-active {
+				text-decoration: underline;
+			}
+		`}</style>
+	</header>
 );
 
 export default withRouter(Header);
--- pages/forms.js
+++ pages/forms.js
@@ -1,12 +1,19 @@
 import App from '../components/App';
 import Header from '../components/Header';
-import { FormList } from '../components/Forms';
+//import FormsAdminApp from '../components/FormAdminApp';
+
+import dynamic from 'next/dynamic';
+
+const FormsAdminApp = dynamic(() => import('../components/FormAdminApp'), {
+	ssr: false,
+});
+
 export default () => (
 	<App>
 		<Header />
 		<article>
 			<h1>Forms</h1>
-			<FormList />
+			<FormsAdminApp />
 		</article>
 	</App>
 );
